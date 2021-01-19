a77fe7e50f64aa506a1c64925ec96482f8b0c397
wahab14131211 <wahab14131211@users.noreply.github.com>
2021-01-19 15:09:43 -0500

    initial submission for provided lab1 files and gitignore to ignore all non .h and .c files (and the gitignore itself)

---
+++ lab1/pri_queue.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "pri_queue.h"
+/** @file pri_queue.c */
+static Node_ptr_t head = NULL;
+/**
+ * Insert a Node into a priority queue.
+ * @param priority
+ * @param data
+ * @author YOUR NAME
+ */
+void PQ_insert(int priority, char * data) {
+ //FIX THIS
+}
+/**
+ * Delete and return the node with the highest priority.
+ * @return The highest priority Node.
+ */
+Node_ptr_t PQ_delete() {
+  //FIX THIS
+    return NULL;
+}
+
+/**
+ * Do NOT modify this function.
+ * @return A pointer to the head of the list.  (NULL if list is empty.)
+ */
+Node_ptr_t PQ_get_head() {
+    return head;
+}
+
+/**
+ * Do NOT modify this function.
+ * @return the number of items in the queue
+ */
+int PQ_get_size() {
+    int size = 0;
+    Node_ptr_t tmp;
+    for(tmp = head; tmp != NULL; tmp = tmp->next, size++)
+        ;
+    return size;
+}
+
+
