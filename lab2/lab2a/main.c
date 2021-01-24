b8eb1638cb076e6fa63271a547b20aa364c0d237
wahab14131211 <wahab14131211@users.noreply.github.com>
2021-01-24 00:10:15 -0500

    Initial submission for provided files for lab2

---
+++ lab2/lab2a/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>  //printf, fprintf
+#include <stdlib.h> //EXIT_SUCCESS
+#include <string.h> //strlen, strcmp
+#define TOO_MANY_ARGS 2
+#define TOO_FEW_ARGS 1
+
+int main(int argc, char* argv[]) {
+    //Default values:
+    int exit_code = EXIT_SUCCESS;
+    char * greeting = "Hello";
+    char * person = "UNKNOWN";
+    //Add code to change the exit_code depending on argc
+    
+    
+    //Add code to change 'person' if one is given on command line
+    
+    int len = strlen(argv[0]);
+    char * last3 = argv[0] + len - 3; //last3 points to last 3 chars
+    fprintf(stderr, "%s\n", argv[0]);
+    fprintf(stderr, "%s\n", last3);
+    
+    //Add code to change 'greeting' if last 3 chars of command are 'bye'
+  
+    printf("%s %s\n", greeting, person);
+    
+    fprintf(stderr, "exit_code: %d\n", exit_code);
+    
+    return exit_code; //Could also say exit(exit_code)
+}
