844e1181f7df773f66507c5349c8e726ce3cb4bc
wahab14131211 <wahab14131211@users.noreply.github.com>
2021-02-15 00:08:45 -0500

    compleated lab4 using alternative approach from previous commit

--- lab4/lab4/main.c
+++ lab4/lab4/main.c
@@ -22,105 +22,38 @@
  * 
  */
 int main(int argc, char** argv) {
-    char *parent_input, *child_input;
-    do{
-        char *parent_argv[100], *child_argv[100];
-        parent_input=(char *)malloc(100*sizeof(char));
-        child_input=(char *)malloc(100*sizeof(char));
-        strncpy(parent_input,"\0",100);
-        strncpy(child_input,"\0",100);
-        int parent_input_counter=0,child_input_counter=0,parent_argv_counter=0, child_argv_counter=0, ch, parent_or_child=0;
-        printf("Your Command>");
-        while((ch=getchar()) != '\n'){
-            if(parent_input_counter==0){
-                parent_argv[parent_argv_counter]=parent_input;
-                *(parent_input+parent_input_counter)=ch;
-                parent_input_counter++;
-                parent_argv_counter++;
-            }else if(ch=='|'){
-                parent_or_child=1;
-            }else if(ch==' '){
-                if(!parent_or_child){
-                    parent_argv[parent_argv_counter]=parent_input+parent_input_counter+1;
-                    *(parent_input+parent_input_counter)='\0';
-                    parent_input_counter++;
-                    parent_argv_counter++;
-                }else{
-                    if(child_input_counter==0){
-                        child_argv[child_argv_counter]=child_input;
-                        *(child_input+child_input_counter)=ch;
-                        child_input_counter++;
-                        child_argv_counter++;
-                    }else{
-                        child_argv[child_argv_counter]=child_input+child_input_counter+1;
-                        *(child_input+child_input_counter)='\0';
-                        child_input_counter++;
-                        child_argv_counter++;
-                    }
-                }
-            }else{
-                if(!parent_or_child){
-                    *(parent_input+parent_input_counter)=ch;
-                    parent_input_counter++;
-                }else{
-                    *(child_input+child_input_counter)=ch;
-                    child_input_counter++;
-                }
-            }
-        }
-        *(parent_input+parent_input_counter)='\0';
-        *(child_input+child_input_counter)='\0';
-        parent_argv[parent_argv_counter]=NULL;
-        child_argv[child_argv_counter]=NULL;
+    char *parent_command, *child_command;
+    parent_command=(char *)malloc(100*sizeof(char));
+    child_command=(char *)malloc(100*sizeof(char));
+    printf("Your Command>");
+    fgets(parent_command,100,stdin);
+    child_command=strchr(parent_command,'|')+1; //identify the pipe
+    if(child_command-1==NULL){ //check to see if there is a pipe
+        fprintf(stderr,"No Pipe Found");
+        exit(1);
+    }
+    for(;child_command[0]==' ';child_command=child_command+1); //remove any spaces between pipe and the child command
+    for(char *temp=strchr(parent_command,'|')-1;temp[0]==' ';temp[0]='\0',temp--); //remove any spaces between pipe and the parent command
 
-        //since exec will replace the current process image with a new image, and we need to run exec in the "parent", we will have 3 levels of processes
-        //1. This process which handles user input and spawns a child process to run the first command
-        //2. This process will be considered the "parent" process and will spawn another child to run the child process
-        //3. runs the child process
+    int pd[2]; //create pipe descriptor
 
-        pid_t pid1 = fork(); //spawn level 2 process as described above
-        if(pid1<0){
-            fprintf(stderr,"Error in creating child process");
-        }
-        if(pid1>0){
-            //level 1 process
-            wait(0);
-            goto outside_fork;
-        }else{
-            //level 2 process
-            int pd[2];
-            if(pipe(pd)==-1){
-                fprintf(stderr,"Error in creating pipe");
-            }
-            pid_t pid2=fork(); //spawn level 3 process as described above
-            if(pid2<0){
-                //error in creating level 3 process
-                fprintf(stderr,"Error in creating child process");
-            }else if(pid2>0){
-                //level 2 process, needs to run parent command
-                dup2(pd[1],STDOUT_FILENO);
-                close(pd[0]);
-                close(pd[1]);
-                execv(parent_input, parent_argv);
-                exit(0);
-            }else{
-                //level 3 process, needs to run child command
-                dup2(pd[0],STDIN_FILENO);
-                close(pd[1]);
-                close(pd[0]);
-                execv(child_input, child_argv);
-                exit(0);
-            }
-            
-        }
+    if(pipe(pd)<0){ //create pipe and check for errors
+        fprintf(stderr,"Error in creating pipe");
+        exit(1);
+    }
 
-        outside_fork:
-        printf("parent_input is: %s\nparent_argv1 is: %s\nparent_argv2 is: %s\nchild_input is: %s\nchild_argv1 is: %s\nchild_argv2 is: %s\n",parent_input,parent_argv[0],parent_argv[1],child_input,child_argv[0],child_argv[1]);
-        while(wait(NULL)>0);
-        sleep(1);
-        fflush(stdin);
-    }while(strncmp(parent_input,"exit",4)!=0);
-    printf("GoodBye");
-    return (EXIT_SUCCESS);
+    if(fork()==0){
+        //child process
+        dup2(pd[0],0);
+        close(pd[1]);
+        system(child_command);
+        exit(0);
+    }else{
+        //parent process
+        dup2(pd[1],1);
+        close(pd[0]);
+        system(parent_command);
+    }
+    return(0);
 }
 
