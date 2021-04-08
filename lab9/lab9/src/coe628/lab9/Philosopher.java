cdc659d66870852871b278d744ec14092fd51cd7
wahab14131211 <wahab14131211@users.noreply.github.com>
2021-04-07 22:49:47 -0400

    Cleaned up output for lab9

--- lab9/lab9/src/coe628/lab9/Philosopher.java
+++ lab9/lab9/src/coe628/lab9/Philosopher.java
@@ -12,7 +12,7 @@ import coe628.lab9.Diner;
  */
 public class Philosopher extends Thread{
 
-    private enum phil_state {THINKING, HUNGRY, EATING};
+    private enum phil_state {Thinking, HasOneFork, Eating};
     private final int id;
     public phil_state state;
     private final Semaphore s;
@@ -20,16 +20,16 @@ public class Philosopher extends Thread{
     public Philosopher(int id){
         this.id = id;
         s = new Semaphore(0);
-        state=phil_state.THINKING;
+        state=phil_state.Thinking;
     }
     
     public void getFork(){
-        this.state = phil_state.HUNGRY;
+        this.state = phil_state.HasOneFork;
         printState();
-        while(state == phil_state.HUNGRY){
+        while(state == phil_state.HasOneFork){
             Diner.mutex.down();
-            if(state == phil_state.HUNGRY && Diner.left(this.id).state != phil_state.EATING && Diner.right(this.id).state != phil_state.EATING){
-                state = phil_state.EATING;
+            if(state == phil_state.HasOneFork && Diner.left(this.id).state != phil_state.Eating && Diner.right(this.id).state != phil_state.Eating){
+                state = phil_state.Eating;
                 Diner.mutex.up();
             }
             Diner.mutex.up();
@@ -39,12 +39,12 @@ public class Philosopher extends Thread{
     
     public void putFork(){
         Diner.mutex.down();
-        this.state = phil_state.THINKING;
+        this.state = phil_state.Thinking;
         think();
-        if(Diner.left(this.id).state == phil_state.HUNGRY){
+        if(Diner.left(this.id).state == phil_state.HasOneFork){
             Diner.left(this.id).s.up();
         }
-        if(Diner.right(this.id).state == phil_state.HUNGRY){
+        if(Diner.right(this.id).state == phil_state.HasOneFork){
             Diner.right(this.id).s.up();
         }
         Diner.mutex.up();
